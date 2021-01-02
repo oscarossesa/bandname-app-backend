d04cee6dbc698cd82b5d45563ee0e034710dfb7a
Oscar Osses <oscarossesa@gmail.com>
2021-01-02 20:54:52 -0300

    first commit

---
+++ models/server.js
@@ -0,0 +1,51 @@
+// Servidor de Express
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const path = require('path')
+const Sockets = require('./sockets')
+
+class Server {
+
+  constructor() {
+
+    this.app = express()
+
+    this.port = process.env.PORT
+
+    // Http server
+    this.server = http.createServer(this.app)
+
+    // Configuraciones de sockets
+    this.io = socketio(this.server, { /* configuraciones */ });
+
+  }
+
+  middlewares() {
+
+    // Desplegar el directorio público
+    this.app.use(express.static(path.resolve(__dirname, '../public')))
+
+  }
+
+  configureSockets() {
+
+    new Sockets(this.io)
+  }
+
+  execute() {
+
+    // Inicializar middelwares
+    this.middlewares()
+
+    // Inicializar Sockets
+    this.configureSockets()
+
+    // Inicializar server
+    this.server.listen(this.port, () => {
+      console.log('Server corriendo en puerto:', this.port)
+    });
+  }
+}
+
+module.exports = Server
---
+++ models/sockets.js
@@ -0,0 +1,28 @@
+class Sockets {
+
+  constructor(io) {
+
+    this.io = io
+
+    this.socketEvents()
+
+  }
+
+  socketEvents() {
+
+    // On connection
+    this.io.on('connection', (socket) => {
+
+      // Escuchar evento
+      socket.on('message-to-server', (data) => {
+        console.log(data);
+        this.io.emit('message-from-server', data)
+      })
+
+    })
+
+  }
+
+}
+
+module.exports = Sockets
